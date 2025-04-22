e09067df2b8c2a4ab5f8be6d3bb4ee76609b7793
D'mari Williams <dmariw90@outlook.com>
2025-04-21 20:51:09 -0400

    Update firebaseConfig.js

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -1,14 +1,1 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC5wWvr1qsGE8kmURgYrfSLxRert3f3g8s",
-  authDomain: "wheelbethere-a5fa2.firebaseapp.com",
-  projectId: "wheelbethere-a5fa2",
-  storageBucket: "wheelbethere-a5fa2.firebasestorage.app",
-  messagingSenderId: "281840293560",
-  appId: "1:281840293560:web:252156135dc9298054464d"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export default {firebaseConfig,app};
+//Firebase credential code
